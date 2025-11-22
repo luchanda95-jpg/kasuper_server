0ac6a802874565bc34bdfbc961b6aff6f6a4d101
nyakamadev <lungunyaka@gmail.com>
2025-11-22 13:20:42 +0200

    Add roles middleware

---
+++ middleware/roles.js
@@ -0,0 +1,11 @@
+// middleware/roles.js
+function requireRole(role) {
+  return (req, res, next) => {
+    if (!req.user || req.user.role !== role) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    next();
+  };
+}
+
+module.exports = { requireRole };
